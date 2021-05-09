af02c2a9c9644983b62eeb871b9f7506dae923df
juninhocb <53801149+juninhocb@users.noreply.github.com>
2021-05-08 21:52:09 -0300

    Add files via upload

--- funcs.py
+++ funcs.py
@@ -1,5 +1,8 @@
-def novoUsuario(nome, email, senha):
+def novoUsuario(nome, email):
     return {"nome": nome, "email": email}
 
 def novoDado(nome, endereco, bloco, ap):
     return {"nome": nome,"endereco": endereco, "bloco": bloco, "ap": ap}
+
+def novoComparar(nome):
+    return {"nome": nome}
